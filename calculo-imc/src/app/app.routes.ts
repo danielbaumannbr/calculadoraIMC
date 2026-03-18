33b4cb634e923b4bd7e1be822d294051a5537f82
Daniel Baumann <143436969+danielbaumannbr@users.noreply.github.com>
2026-03-18 14:14:44 -0300

    Add routing and router-outlet
    Replace static app components with a <router-outlet> in app.html and define application routes. app.routes.ts now imports Imc and Teste components and registers routes for 'imc' and 'teste' paths so navigation is handled by the router.

--- calculo-imc/src/app/app.routes.ts
+++ calculo-imc/src/app/app.routes.ts
@@ -1,3 +1,9 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Imc } from './imc/imc';
+import { Teste } from './teste/teste';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'imc',component: Imc},
+    {path:'teste',component: Teste}
+];
