f150cbaa91aed04e3dbb5cbb9ae729c84208f0c0
Daniel Baumann <143436969+danielbaumannbr@users.noreply.github.com>
2026-03-18 14:39:06 -0300

    Add RouterLink and navigation links
    Add two navigation anchors to app.html (<a routerLink="imc"> and <a routerLink="teste">) and register RouterLink in the AppComponent imports. This enables the routerLink directive for in-app navigation between IMC and Teste views.

--- calculo-imc/src/app/app.ts
+++ calculo-imc/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import {Imc} from './imc/imc';
 import {Teste} from './teste/teste';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Imc,Teste],
+  imports: [RouterOutlet,Imc,Teste,RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
